c3dfad9f43ebf41e5eb5ee23f6abc82e73dc25c1
drabinowitz <dmitrirabinowitz+development@gmail.com>
2015-01-17 02:43:09 -0500

    Move input logic into form so that form can manage all state for inputs.

--- Components/Forms/Input.js
+++ Components/Forms/Input.js
@@ -31,66 +31,28 @@ var Input = React.createMutableClass({
   },
 
   nearestOwnerPropTypes: {
-    __tuxHandleIsValidCheck__: React.PropTypes.func.isRequired
+    registerWithForm: React.PropTypes.func.isRequired,
+    unregisterWithForm: React.PropTypes.func.isRequired,
+    checkIfInputIsValid: React.PropTypes.func.isRequired
   },
 
-  getInitialState: function () {
-    return {
-      message: this.props.defaultValue || ''
-    };
+  componentDidMount: function () {
+    this.nearestOwnerProps.registerWithForm(this, this.getDOMNode().value.toString());
   },
 
-  checkIfValid: function (message) {
-    var minLength = this.props.minLength;
-    var maxLength = this.props.maxLength;
-    var minValue = this.props.minValue;
-    var maxValue = this.props.maxValue;
-    var required = this.props.required;
-    var pattern = this.props.pattern;
-    var customCheck = this.props.customCheck;
-    //TODO CHANGE TO RETURN ACTUAL ERRORS
-    if (minLength !== undefined && minLength > message.length) {
-      return false;
-    }
-
-    if (maxLength !== undefined && maxLength < message.length) {
-      return false;
-    }
-
-    if (minValue !== undefined && minValue > +message) {
-      return false;
-    }
-
-    if (maxValue !== undefined && maxValue < +message) {
-      return false;
-    }
-
-    if (required !== undefined && !message.length) {
-      return false;
-    }
-
-    if (pattern !== undefined && message.search(pattern) === -1) {
-      return false;
-    }
-
-    if (customCheck !== undefined && !customCheck(message)) {
-      return false;
-    }
-
-    return true;
+  componentWillUnmount: function () {
+    this.nearestOwnerProps.unregisterWithForm(this);
   },
 
   handleChange: function (event) {
     var newMessage = event.target.value.toString();
-    var isValid = this.checkIfValid(newMessage);
-    this.nearestOwnerProps.__tuxHandleIsValidCheck__(this.props.name, isValid);
+    this.tuxIsValid = this.nearestOwnerProps.checkIfInputIsValid(this, newMessage);
   },
 
   render: function () {
     var type = this.props.type || 'text';
-    var message = this.state.message;
     return (
-      <input type={type} value={message} onChange={this.handleChange} />
+      <input type={type} onChange={this.handleChange} />
     );
   }
 });
--- Components/Forms/index.js
+++ Components/Forms/index.js
@@ -1,22 +1,127 @@
 'use strict';
 
 var React = require('tux/React');
+var assign = require('object-assign');
+var invariant = require('tux/src/TuxInvariant');
 
 var Form = React.createOwnerClass({
-  registerOwnerProps: function () {
+  getInitialState: function () {
     return {
-      __tuxHandleIsValidCheck__: function (inputName, isValid) {
-        var newState = {
-          validInputMap: {}
-        };
-        newState.validInputMap[inputName] = isValid;
-        this.extendState(newState);
-      }.bind(this)
+      validInputMap: {}
     };
   },
 
+  checkIfInputIsValid: function (input, message) {
+    var newState = {
+      validInputMap: assign({}, this.state.validInputMap)
+    };
+
+    var validityMap = this.generateValidityMapForInput(input, message);
+    newState.validInputMap[input.props.name] = validityMap;
+    this.setState(newState);
+  },
+
+  registerWithForm: function (input, message) {
+    var newState = {
+       validInputMap: assign({}, this.state.validInputMap)
+    };
+
+    var validInputMap = newState.validInputMap;
+    var name = this.props.name;
+    invariant(!validInputMap[name], 'input name: "%s" is already registered in this form.  Form inputs must be unique.', name);
+    this.checkIfInputIsValid(input, message);
+  },
+
+  unregisterWithForm: function (input) {
+    var newState = {
+       validInputMap: assign({}, this.state.validInputMap)
+    };
+
+    delete newState.validInputMap[input.props.name];
+    this.setState(newState);
+  },
+
+  checkIfFormIsValid: function () {
+    var input;
+    var validInputMap = this.state.validInputMap;
+    var inputValueMap = {};
+    for (input in validInputMap) {
+      if (validInputMap.hasOwnProperty(input)) {
+        var validityMap = validInputMap[input];
+        if (!validityMap.isValid) {
+          return false;
+        }
+        inputValueMap[input] = validityMap.value;
+      }
+    }
+    return inputValueMap;
+  },
+
+  registerOwnerProps: function () {
+    return {
+      checkIfInputIsValid: this.checkIfInputIsValid.bind(this),
+      checkIfFormIsValid: this.checkIfFormIsValid.bind(this),
+      registerWithForm: this.registerWithForm.bind(this),
+      unregisterWithForm: this.unregisterWithForm.bind(this)
+    };
+  },
+
+  generateValidityMapForInput: function (input, message) {
+    var minLength = input.props.minLength;
+    var maxLength = input.props.maxLength;
+    var minValue = input.props.minValue;
+    var maxValue = input.props.maxValue;
+    var required = input.props.required;
+    var pattern = input.props.pattern;
+    var customCheck = input.props.customCheck;
+
+    var validityMap = {value: message};
+    if (minLength !== undefined && minLength > message.length) {
+      validityMap.minLength = true;
+    }
+
+    if (maxLength !== undefined && maxLength < message.length) {
+      validityMap.maxLength = true;
+    }
+
+    if (minValue !== undefined && minValue > +message) {
+      validityMap.minValue = true;
+    }
+
+    if (maxValue !== undefined && maxValue < +message) {
+      validityMap.maxValue = true;
+    }
+
+    if (required !== undefined && !message.length) {
+      validityMap.required = true;
+    }
+
+    if (pattern !== undefined && message.search(pattern) === -1) {
+      validityMap.pattern = true;
+    }
+
+    if (customCheck !== undefined && !customCheck(message)) {
+      validityMap.customCheck = true;
+    }
+
+    validityMap.isValid = Object.keys(validityMap).length > 1;
+
+    return validityMap;
+  },
+
+  handleSubmit: function () {
+    var inputValueMap = this.checkIfFormIsValid();
+    if (inputValueMap) {
+      this.props.handleSubmit(inputValueMap);
+    }
+  },
+
   render: function () {
-    React.children.map
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        {this.props.children}
+      </form>
+    );
   }
 });
 
